70e7bbf65fe8631d9fd80f3a6c71eff15b006968
Dan Esparza <esparza.dan@gmail.com>
2020-01-09 14:00:03 -0500

    Starting app styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../src/App.css';
 
-function App() {
+function App() {    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+        <h1 className="title">Denominations</h1>
+        <h2 className="subtitle">
+            A simple app designed to give you the right <strong>money denominations</strong> automagically
+        </h2>
+
+        <div className="field">
+            <p className="control has-icons-left">
+                <input className="input" type="email" placeholder="Amount" />
+                <span className="icon is-small is-left">
+                    <i className="fas fa-dollar-sign"></i>
+                </span>
+            </p>
+        </div>
+
+        <div class="control">
+            <button class="button is-primary">
+                <span class="icon">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span>More</span>
+            </button>
+        </div>
+        
     </div>
   );
 }
