8dd3579fca70c9034c9eb758913683e66dfcd43c
Dan Esparza <esparza.dan@gmail.com>
2020-01-15 14:53:41 -0500

    Updates styles

--- src/App.js
+++ src/App.js
@@ -10,35 +10,58 @@ function App() {
         <h2 className="subtitle">
             A simple app designed to give you the right <strong>money denominations</strong> automagically
         </h2>
-
-        <div className="field">
-            <p className="control has-icons-left">
-                <input className="input" type="number" placeholder="Amount" />
-                <span className="icon is-small is-left">
-                    <i className="fas fa-dollar-sign"></i>
-                </span>
-            </p>
-            
-            <p className="control">
-                Include  &nbsp;
-
-                <label class="checkbox">
-                    <input type="checkbox" checked="true" />
-                    100's
-                </label>
-
-                <label class="checkbox">
-                    <input type="checkbox" checked="true" />
-                    20's
-                </label>
-            </p>
-            
+        
+        <div className="field is-horizontal">            
+            <div className="field-body">
+                <div className="field has-addons">
+                    <div className="control is-expanded has-icons-left">
+                        <input className="input" type="number" placeholder="Amount" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-dollar-sign"></i>
+                        </span>
+                    </div>
+                    <div className="control">
+                        <button className="button is-danger">Remove</button>
+                    </div>
+                </div>                
+            </div>
         </div>
 
-        <div class="control">
-            <button class="button is-primary">
-                <span class="icon">
-                    <i class="fas fa-plus"></i>
+        <div className="field is-horizontal">
+            <div className="field-body">
+                <div className="field is-expanded">
+                    <div className="field">
+                        <p className="control">
+                        Include: 
+                        <label class="checkbox">
+                            <input type="checkbox" className="includeOption" checked="true" />
+                            100's
+                        </label>
+
+                        <label class="checkbox">
+                            <input type="checkbox" className="includeOption" checked="true" />
+                            20's
+                        </label>
+
+                        <label class="checkbox">
+                            <input type="checkbox" className="includeOption" checked="true" />
+                            10's
+                        </label>
+
+                        <label class="checkbox">
+                            <input type="checkbox" className="includeOption" checked="true" />
+                            5's
+                        </label>
+                        </p>                        
+                    </div>                    
+                </div>
+            </div>
+        </div>
+
+        <div className="control">
+            <button className="button is-primary">
+                <span className="icon">
+                    <i className="fas fa-plus"></i>
                 </span>
                 <span>More</span>
             </button>
